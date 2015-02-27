5383a37807fa99810cd9206d7f276882b87345c1
culmou1 <daehlinadeauotis@gmail.com>
2015-02-27 14:24:51 -0500

    Initial Commit

---
+++ tp2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  tp2
+//
+//  Created by Daehli Nadeau Otis on 2015-02-27.
+//  Copyright (c) 2015 Daehli Nadeau-Otis. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
