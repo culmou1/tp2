5f034b4b38603f4b5983b7f6842f45464d15b89f
gchaville <gillian.chaville@gmail.com>
2015-03-12 18:41:47 -0400

    Update Header.h

--- tp2/Header.h
+++ tp2/Header.h
@@ -12,18 +12,27 @@
 #include <stack>
 #include <algorithm>
 
-class PREFIXE {
-private:
-    std :: stack <char> Pile;
-    std :: vector <char> Tableau;
-public :
-    PREFIXE();// constructeur ;
-    ~PREFIXE();// destructeur ;
-    bool lecture(std::vector<char> Tableau );// lit l’expression à évaluer, à partir du clavier, dans tableau et valide si l’expression ne contient que les caractères ci-dessus, à savoir les nombres entiers composés de caractères nombres, et les opérateurs cités ci-dessus.
-    bool valider_expression (std ::vector<char> Tableau); // teste si l’expression est bien définie (le nombre de parenthèses ouvrantes et fermantes est le même).
-    void s (std::vector <char> Tableau); // transforme les nombres lus en caractères en valeurs numériques
-    void transformerenpostfixe(std ::stack<char> Pile, std::vector<char> Tableau);// transforme l’expression lue en une expression prefixée et l’afficher.
-    int evaluer_expression(std::stack<char> Pile, std::vector<char> Tableau); // évalue l’expression préfixée et affiche sa valeur.
+class Prefixe {
+	private :
+		std::stack <std::string> Pile;
+		std::vector <std::string> Tableau;
+
+	public : 
+		Prefixe(); //constructeur 
+		~Prefixe (); //destructeur 
+		bool lecture(std::vector<std::string> tableau); 
+		/* lit l’expression à évaluer, à partir du clavier, dans tableau et valide si l’expression ne contient 
+		que les caractères ci-dessus, à savoir les nombres entiers composés de caractères nombres, et les 
+		opérateurs cités ci-dessus.*/
+		bool valider_expression (std::vector<std::string> tableau); 
+		/* teste si l’expression est bien définie (le nombre de parenthèses ouvrantes et fermantes est le même).*/
+		void transformerennombres (std::vector <std::string> tableau); 
+		/* transforme les nombres lus encaractères en valeurs numériques*/
+		void transformerenprefixe(std::stack<std::string> Pile, std::vector<std::string> tableau);
+		/*transforme l’expression lue en une expression prefixée et l’afficher.*/
+		int evaluer_expression(std::stack<std::string> Pile, std::vector<std::string> tableau); 
+		// évalue l’expression préfixe
 };
+
 
 #endif
