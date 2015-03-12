fdb29a7a5c2986c159ab3461b4f301652e93b632
gchaville <gillian.chaville@gmail.com>
2015-03-12 18:40:19 -0400

    reussi main.cpp

--- tp2/main.cpp
+++ tp2/main.cpp
@@ -1,113 +1,48 @@
-//
-//  main.cpp
-//  tp2
-//
-//  Created by Daehli Nadeau Otis on 2015-02-27.
-//  Copyright (c) 2015 Daehli Nadeau-Otis. All rights reserved.
-//
-
 #include <iostream>
-#include <cstdlib> // Bibliothèque spécial
-#include "Header.h"
+#include "Prefixe.h"
+#include <string>
+#include <cstdlib>
+#include <sstream>
 using namespace std;
 
+bool estNombre (string t);
+bool estOperande (string t);
+int precedance(string t);
 
-PREFIXE :: PREFIXE()
-{
+Prefixe::Prefixe() {
 }
-bool PREFIXE :: lecture(vector<char> Tableau)
+
+Prefixe::~Prefixe () {
+}
+
+
+bool Prefixe :: lecture(vector<string> tableau)
 {
-    bool lectureCar = false; // Vérifie si les caractères sont OK
+    bool lectureToken =false; // Vérifie si les caractères sont OK
     
-    for (int i=0; i<Tableau.size(); i++)
+    for (int i=0; i<tableau.size(); i++)
     {
-        switch (Tableau[i])
-        {
-            case '(':
-                cout << "Lisible";
-                lectureCar = true;
-                break;
-            case ')':
-                cout << "Lisible";
-                lectureCar = true;
-                break;
-            case '*':
-                cout << "Lisible";
-                lectureCar = true;
-                break;
-            case '/':
-                cout << "Lisible";
-                lectureCar = true;
-                break;
-            case '%':
-                cout << "Lisible";
-                lectureCar = true;
-                break;
-            case '+':
-                cout << "Lisible";
-                lectureCar = true;
-                break;
-            case '-':
-                cout << "Lisible";
-                lectureCar = true;
-                break;
-            case '0':
-                cout << "Lisible";
-                lectureCar = true;
-                break;
-            case '1':
-                cout << "Lisible";
-                lectureCar = true;
-                break;
-            case '2':
-                cout << "Lisible";
-                lectureCar = true;
-                break;
-            case '3':
-                cout << "Lisible";
-                lectureCar = true;
-                break;
-            case '4':
-                cout << "Lisible";
-                lectureCar = true;
-                break;
-            case '5':
-                cout << "Lisible";
-                lectureCar = true;
-                break;
-            case '6':
-                cout << "Lisible";
-                lectureCar = true;
-                break;
-            case '7':
-                cout << "Lisible";
-                lectureCar = true;
-                break;
-            case '8':
-                cout << "Lisible";
-                lectureCar = true;
-                break;
-            case '9':
-                cout << "Lisible";
-                lectureCar = true;
-                break;
-            default:
-                cerr << "Vous n'avez aucun paramêtre valide" << endl;
-                return false;
-        }
-    }
-    return true;
+       if (estNombre (tableau[i]) || estOperande (tableau[i]))
+	   {
+			lectureToken  = true;
+			Tableau.push_back(tableau[i]);
+	   }
+		else 
+			return false;
+	}
+	return lectureToken;
 }
-bool PREFIXE :: valider_expression (vector<char> Tableau)
+
+bool Prefixe :: valider_expression (vector<string> tableau)
 {
     int pargauche = 0, pardroite = 0; // Calculer le nombre de parenthèse;
-    for (int i = 0; i <Tableau.size(); i++)
+    for (int i = 0; i <tableau.size(); i++)
     {
-        if (Tableau[i]== '(')
+        if (tableau[i] == "(")
         {
             pargauche += 1; // COMPTE LE NOMBRES DE PARATHÈSE
         }
-        else if (Tableau[i]== ')')
+        else if (tableau[i]== ")")
         {
             pardroite += 1;
         }
@@ -118,36 +53,104 @@ bool PREFIXE :: valider_expression (vector<char> Tableau)
       return false;
 }
 
-void PREFIXE:: transformerennombres (vector <char> Tableau)
+void Prefixe:: transformerennombres (vector <string> tableau)
 {
-   int temp;
-    for (int i = 0; i<Tableau.size(); i++) 
-	{
-		if (Tableau[i] >= '0' && Tableau[i] <= '9')
-		{
-			temp = atoi(&Tableau[i]);
-			Tableau[i] = temp;
-		}
-		cout << Tableau[i];
-    }
+	vector <int> operande;
+
+	for (int i= 0; i < tableau.size(); i++) {
+		if (estNombre (tableau[i]))
+			operande.push_back(stoi(tableau[i]));
+	}
+   
     /* J'ai essayé la focntion atoi. Elle reconnait un nombre meme chacun de ses chiffres est sur un digit.
     Par exemple, si on a : tableau[0]= 2, tableau[1]= 5 et tableau[2]= 5. Dès qu'elle arrive à tableau[0]
     elle a lire 255 et ensuite ça ressort comme ça : tableau[0]= 255, tableau[1]= 5 et tableau[2]= 5.
     Faudrait supprimer les digits qui suivent j'y travaille. */
 }
 
-int main()
+void Prefixe:: transformerenprefixe(stack<string> Pile, vector<string> tableau)
 {
-    PREFIXE premier;
-    string entre;
-    vector <char> expression;
+    stack <string> operande;
+    stack <string> operateur;
     
-    cout << "Entrer une équations";
-    cin >> entre;
+	for (int i = tableau.size()-1; i >=0;i--)
+    {
+        if (estNombre (tableau[i])) {
+            operande.push(tableau[i]);// Ajoute un élément à la pile operande comme 3, 4, 5,6,
+            Pile.push(tableau[i]);
+        }
+		
+		if (estOperande (tableau[i])) {
+			if (tableau[i] == ")")
+            operateur.push(tableau[i]); // Ajoute un element à la pile des operateur +,-,\,%
+			else if (tableau[i] == "(") {
+				while (operateur.top() != ")") {
+					Pile.push(operateur.top());
+					operateur.pop();
+				}
+				operateur.pop();
+			}
+			else {
+				bool pushed = false;
+				while (!pushed) {
+					if (operateur.empty() || operateur.top() == ")" || precedance(tableau[i]) >= precedance(operateur.top())) {
+					operateur.push(tableau[i]);
+					pushed =true;
+					}
+					else {
+						Pile.push(operateur.top());
+						operateur.pop();
+					}
+				}
+			}
+		}
+		
+    }
+	while (!operateur.empty()) {
+		Pile.push(operateur.top());
+		operateur.pop();
+	}
     
+    while (!Pile.empty())
+    {
+		cout << " " << Pile.top();
+		Pile.pop();
+	}
+}
+        
+
+int Prefixe:: evaluer_expression(stack<string> Pile, vector<string> Tableau)
+{
+    // Il faut peut-être mettre les opérateurs dans une pile et laisser les opérande dans un tableau.
+    // et à la fin nous avons juste à dépilé la pile et le tableau. Il est maintenant l'heure d'aller en cours.
+    // À tanto ..
+}
+
+void main()
+{
+    Prefixe premier;
+    string entree;
+	int i = 0;
+    vector <string> expression;
+	stack <string> prefixe;
+    
+    cout << "Entrer une équation : ";
+    cin >> entree;
     // pour faire appel au fonction on a besion d'un tableau
-    for(int i =0; i<entree.size(); i++) {
-		expression.push_back(entree[i]);
+   while ( i <entree.size()) {
+	   string temp;
+	   if (entree[i]>= '0' && entree[i] <= '9') {
+		   while (entree[i]>= '0' && entree[i] <= '9') {
+			   temp += entree[i];
+			   i++;
+		   }
+		   expression.push_back(temp);
+	   }
+	   else {
+		   temp = entree[i];
+		   expression.push_back (temp);
+		   i++;
+	   }
 	}
 
 	if (premier.lecture(expression))
@@ -160,7 +163,30 @@ int main()
 	else
 	   cout << endl << "L'expression manque de parentheses" << endl;
 
-	premier.transformerennombres (expression);
+   premier.transformerenprefixe (prefixe, expression);
+
 
 	system ("pause");
+}
+
+bool estOperande (string t) {
+	return (t == "+" || t == "-" || t == "*" || t == "/" || t == "%" || t == "(" || t == ")");
+}
+
+bool estNombre (string t) {
+	for (int i = 0; i < t.size(); i++) {
+		if (t[i] < '0' || t[i] > '9')
+			return false;
+	}
+	return true;
+}
+
+int precedance(string t)
+{
+    int temp;
+	if (t == "*" || t == "/" || t == "%")
+       temp = 2;
+    else  if (t == "+" || t == "-")
+       temp = 1;
+    return temp;
 }
