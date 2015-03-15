cc0cb07273dea2618008ecb9d43f0fda0fa964ed
gchaville <gillian.chaville@gmail.com>
2015-03-14 20:25:22 -0400

    Update Header.h

--- tp2/Header.h
+++ tp2/Header.h
@@ -14,25 +14,24 @@
 
 class Prefixe {
 	private :
-		std::stack <std::string> Pile;
-		std::vector <std::string> Tableau;
+		std::stack <std::string> *Pile;
+		std::vector <int> *Tableau;
 
 	public : 
 		Prefixe(); //constructeur 
 		~Prefixe (); //destructeur 
-		bool lecture(std::vector<std::string> tableau); 
+		bool lecture(std::vector<std::string> infixe); 
 		/* lit l’expression à évaluer, à partir du clavier, dans tableau et valide si l’expression ne contient 
 		que les caractères ci-dessus, à savoir les nombres entiers composés de caractères nombres, et les 
 		opérateurs cités ci-dessus.*/
-		bool valider_expression (std::vector<std::string> tableau); 
+		bool valider_expression (std::vector<std::string> infixe); 
 		/* teste si l’expression est bien définie (le nombre de parenthèses ouvrantes et fermantes est le même).*/
-		void transformerennombres (std::vector <std::string> tableau); 
+		void transformerennombres (std::vector<std::string> tokens); 
 		/* transforme les nombres lus encaractères en valeurs numériques*/
-		void transformerenprefixe(std::stack<std::string> Pile, std::vector<std::string> tableau);
+		void transformerenprefixe(std::stack<std::string> *prefixe, std::vector<std::string> infixe);
 		/*transforme l’expression lue en une expression prefixée et l’afficher.*/
-		int evaluer_expression(std::stack<std::string> Pile, std::vector<std::string> tableau); 
+		int evaluer_expression(std::stack<std::string> prefixe, std::vector<std::string> infixe); 
 		// évalue l’expression préfixe
 };
-
 
 #endif
