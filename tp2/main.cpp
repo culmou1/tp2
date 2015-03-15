0617df7c947f6337f615b08a62cebc2fcc0200da
gchaville <gillian.chaville@gmail.com>
2015-03-14 20:23:58 -0400

    Le main qui fonctionne

--- tp2/main.cpp
+++ tp2/main.cpp
@@ -6,43 +6,45 @@
 using namespace std;
 
 bool estNombre (string t);
-bool estOperande (string t);
+bool estOperateur (string t);
 int precedance(string t);
+int operation (string operateur, int ope_gauche, int ope_droite);
 
 Prefixe::Prefixe() {
+	Pile = new stack <string>;
+	Tableau = new vector <int>;
 }
 
 Prefixe::~Prefixe () {
+	delete Pile;
+	delete Tableau;
 }
 
 
-bool Prefixe :: lecture(vector<string> tableau)
+bool Prefixe :: lecture(vector<string> infixe)
 {
     bool lectureToken =false; // Vérifie si les caractères sont OK
     
-    for (int i=0; i<tableau.size(); i++)
+    for (int i=0; i<infixe.size(); i++)
     {
-       if (estNombre (tableau[i]) || estOperande (tableau[i]))
-	   {
+       if (estNombre (infixe[i]) || estOperateur (infixe[i]))
 			lectureToken  = true;
-			Tableau.push_back(tableau[i]);
-	   }
 		else 
 			return false;
 	}
 	return lectureToken;
 }
 
-bool Prefixe :: valider_expression (vector<string> tableau)
+bool Prefixe :: valider_expression (vector<string> infixe)
 {
     int pargauche = 0, pardroite = 0; // Calculer le nombre de parenthèse;
-    for (int i = 0; i <tableau.size(); i++)
+    for (int i = 0; i <infixe.size(); i++)
     {
-        if (tableau[i] == "(")
+        if (infixe[i] == "(")
         {
             pargauche += 1; // COMPTE LE NOMBRES DE PARATHÈSE
         }
-        else if (tableau[i]== ")")
+        else if (infixe[i]== ")")
         {
             pardroite += 1;
         }
@@ -53,39 +55,35 @@ bool Prefixe :: valider_expression (vector<string> tableau)
       return false;
 }
 
-void Prefixe:: transformerennombres (vector <string> tableau)
+void Prefixe::transformerennombres (vector<string> tokens)
 {
-	vector <int> operande;
-
-	for (int i= 0; i < tableau.size(); i++) {
-		if (estNombre (tableau[i]))
-			operande.push_back(stoi(tableau[i]));
+    for (int i=tokens.size()-1; i >= 0; i--) {
+		Tableau->push_back (stoi (tokens[i]));
 	}
-   
     /* J'ai essayé la focntion atoi. Elle reconnait un nombre meme chacun de ses chiffres est sur un digit.
     Par exemple, si on a : tableau[0]= 2, tableau[1]= 5 et tableau[2]= 5. Dès qu'elle arrive à tableau[0]
     elle a lire 255 et ensuite ça ressort comme ça : tableau[0]= 255, tableau[1]= 5 et tableau[2]= 5.
     Faudrait supprimer les digits qui suivent j'y travaille. */
 }
 
-void Prefixe:: transformerenprefixe(stack<string> Pile, vector<string> tableau)
+void Prefixe:: transformerenprefixe(stack<string> *prefixe, vector<string> infixe)
 {
     stack <string> operande;
     stack <string> operateur;
     
-	for (int i = tableau.size()-1; i >=0;i--)
+	for (int i = infixe.size()-1; i >=0;i--)
     {
-        if (estNombre (tableau[i])) {
-            operande.push(tableau[i]);// Ajoute un élément à la pile operande comme 3, 4, 5,6,
-            Pile.push(tableau[i]);
+        if (estNombre (infixe[i])) {
+            operande.push(infixe[i]);// Ajoute un élément à la pile operande comme 3, 4, 5,6,
+            Pile->push(infixe[i]);
         }
 		
-		if (estOperande (tableau[i])) {
-			if (tableau[i] == ")")
-            operateur.push(tableau[i]); // Ajoute un element à la pile des operateur +,-,\,%
-			else if (tableau[i] == "(") {
+		if (estOperateur (infixe[i])) {
+			if (infixe[i] == ")")
+            operateur.push(infixe[i]); // Ajoute un element à la pile des operateur +,-,\,%
+			else if (infixe[i] == "(") {
 				while (operateur.top() != ")") {
-					Pile.push(operateur.top());
+					Pile->push(operateur.top());
 					operateur.pop();
 				}
 				operateur.pop();
@@ -93,12 +91,12 @@ void Prefixe:: transformerenprefixe(stack<string> Pile, vector<string> tableau)
 			else {
 				bool pushed = false;
 				while (!pushed) {
-					if (operateur.empty() || operateur.top() == ")" || precedance(tableau[i]) >= precedance(operateur.top())) {
-					operateur.push(tableau[i]);
+					if (operateur.empty() || operateur.top() == ")" || precedance(infixe[i]) >= precedance(operateur.top())) {
+					operateur.push(infixe[i]);
 					pushed =true;
 					}
 					else {
-						Pile.push(operateur.top());
+						Pile->push(operateur.top());
 						operateur.pop();
 					}
 				}
@@ -107,23 +105,51 @@ void Prefixe:: transformerenprefixe(stack<string> Pile, vector<string> tableau)
 		
     }
 	while (!operateur.empty()) {
-		Pile.push(operateur.top());
+		Pile->push(operateur.top());
 		operateur.pop();
 	}
     
-    while (!Pile.empty())
+    while (!Pile->empty())
     {
-		cout << " " << Pile.top();
-		Pile.pop();
+		prefixe->push (Pile->top());
+		cout << " " << Pile->top();
+		Pile->pop();
 	}
 }
         
-
-int Prefixe:: evaluer_expression(stack<string> Pile, vector<string> Tableau)
+int Prefixe:: evaluer_expression(stack<string> prefixe, vector<string> infixe)
 {
-    // Il faut peut-être mettre les opérateurs dans une pile et laisser les opérande dans un tableau.
-    // et à la fin nous avons juste à dépilé la pile et le tableau. Il est maintenant l'heure d'aller en cours.
-    // À tanto ..
+    vector <string> tableau_to_convert; 
+	stack <int> Resultat;
+	int tempG, tempD;
+
+	if (prefixe.empty()) 
+			return -1;
+	while (!prefixe.empty()) {
+		
+		for (int i=0; i < 2 && estNombre(prefixe.top()); i++) {
+			tableau_to_convert.push_back(prefixe.top());
+			prefixe.pop();
+		}
+		transformerennombres (tableau_to_convert); 
+		
+		if (!Tableau->empty()) {
+			for (int i=Tableau->size()-1; i >= 0 ; i--) {
+				Resultat.push (Tableau->back());
+				Tableau->pop_back();
+			}
+		}
+		else  {
+			tempG = Resultat.top();
+			Resultat.pop(); 
+			tempD = Resultat.top();
+			Resultat.pop();
+			Resultat.push(operation (prefixe.top(), tempG, tempD));
+			prefixe.pop();
+		}
+		tableau_to_convert.clear();
+	}
+	return Resultat.top();
 }
 
 void main()
@@ -163,13 +189,13 @@ void main()
 	else
 	   cout << endl << "L'expression manque de parentheses" << endl;
 
-   premier.transformerenprefixe (prefixe, expression);
-
+   premier.transformerenprefixe (&prefixe, expression);
+   cout << endl << "Resultat :" << premier.evaluer_expression (prefixe, expression) << endl;
 
 	system ("pause");
 }
 
-bool estOperande (string t) {
+bool estOperateur (string t) {
 	return (t == "+" || t == "-" || t == "*" || t == "/" || t == "%" || t == "(" || t == ")");
 }
 
@@ -189,4 +215,23 @@ int precedance(string t)
     else  if (t == "+" || t == "-")
        temp = 1;
     return temp;
+}
+
+int operation (string operateur, int ope_gauche, int ope_droite) {
+	
+	char signe = operateur[0];
+	switch (signe) {
+		case '+' :	return (ope_gauche + ope_droite); 
+			break;
+		case '-' :	return (ope_gauche - ope_droite); 
+			break;
+		case '*' :	return (ope_gauche * ope_droite); 
+			break;
+		case '/' :	return (ope_gauche / ope_droite); 
+			break;
+		case '%' :  return (ope_gauche % ope_droite); 
+			break;
+		default :	return 0;
+			break;
+	}
 }
